f122dd2009bc1e706562cc2a11e057fa00ae79f2
MrEraxd <MrEraxd@gmail.com>
2022-04-27 21:22:41 +0200

    Added dropdown to CraftingPanelNavbar.

--- src/panels/CraftingPanel/CraftingPanelNavbar.vue
+++ src/panels/CraftingPanel/CraftingPanelNavbar.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
   import BaseDropdown from '../../components/Base/BaseDropdown.vue';
   import BaseButton from '../../components/Base/BaseButton.vue';
+
+  const craftingOptions = ['a', 'b', 'c', 'd', 'f'];
+  const callback = (newValue: string) => {
+    console.log('New value: ', newValue);
+  };
 </script>
 
 <template>
   <div class="crafting-panel-navbar">
-    <BaseDropdown></BaseDropdown>
+    <BaseDropdown
+      :options="craftingOptions"
+      :callback="callback"
+      dropdown-label="crafting name"
+    ></BaseDropdown>
+
     <BaseButton button-name="Add custom"></BaseButton>
   </div>
 </template>
