721fe92010e81fb6c23aa6574bff8cea14c4dfa9
MrEraxd <MrEraxd@gmail.com>
2022-04-30 16:31:32 +0200

    Moved to classes instead of variables for text size

--- src/panels/SwappablePanelSwitch.vue
+++ src/panels/SwappablePanelSwitch.vue
@@ -16,7 +16,7 @@
 <template>
   <div class="the-active-panel-switch">
     <button
-      class="the-active-panel-switch__button"
+      class="the-active-panel-switch__button subtitle subtitle--1"
       :class="{
         'the-active-panel-switch__button--active':
           props.activePanel === 'CraftingPanel',
@@ -27,7 +27,7 @@
     </button>
 
     <button
-      class="the-active-panel-switch__button"
+      class="the-active-panel-switch__button subtitle subtitle--1"
       :class="{
         'the-active-panel-switch__button--active':
           props.activePanel === 'SavedRecipesPanel',
@@ -50,11 +50,19 @@
       color: var(--white);
       display: flex;
       justify-content: flex-start;
+      border: none;
       border-right: 2px solid;
       border-color: hsl(var(--color-cc-grey-30));
+      background-color: transparent;
+      text-transform: uppercase;
       padding-top: 24px;
       padding-right: 4px;
       transition: border-color 0.2s;
+      outline: none;
+
+      &:hover {
+        cursor: pointer;
+      }
 
       &--active {
         border-color: hsl(var(--color-cc-blue-50));
@@ -63,7 +71,6 @@
       span {
         writing-mode: vertical-lr;
         transform: scale(-1, -1);
-        font-size: var(--subtitle-1);
         padding-top: 2px;
       }
     }
