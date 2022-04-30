90871440c4e35fcb65961ca4b22cea0e82cba4e8
MrEraxd <MrEraxd@gmail.com>
2022-04-30 16:30:24 +0200

    Added BaseItemCell

---
+++ src/components/Base/BaseItemCell.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="base-item-cell">
+    <div class="base-item-cell__content"></div>
+  </div>
+</template>
+
+<style lang="postcss">
+  .base-item-cell {
+    display: flex;
+    width: 100%;
+    aspect-ratio: 1;
+    background-color: hsl(var(--color-cc-black));
+    transition: outline-color 0.2s;
+    outline: 1px solid transparent;
+    justify-content: center;
+    align-items: center;
+    padding: 4px;
+
+    &:hover {
+      outline-color: hsl(var(--color-cc-blue-50)) !important;
+      cursor: pointer;
+    }
+  }
+</style>
